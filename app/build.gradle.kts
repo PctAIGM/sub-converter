0e413dc1f1d972440fea16f1aa281ffe007b1857
PctAIGM <151592267+PctAIGM@users.noreply.github.com>
2026-06-22 17:52:24 +0800

    bump v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = (System.getenv("VERSION_CODE") ?: "1").toInt()
-        versionName = System.getenv("VERSION_NAME") ?: "0.1.2"
+        versionName = System.getenv("VERSION_NAME") ?: "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
